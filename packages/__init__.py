58d1ef1bbe3afdf21f8e7ac1a6bee523ed892795
Pranuthi23 <61027180+Pranuthi23@users.noreply.github.com>
2026-05-04 11:43:43 -0500

    Delete packages directory

--- packages/__init__.py
+++
@@ -1,8 +0,0 @@
-PACKAGE_DICT = {
-    "EinsumNet":"simple_einet",
-    "RatSPN":"RatSPN",
-    "MultiBench":"MultiBench",
-    "Radiate":"radiate_sdk",
-    "probmetrics":"probmetrics",
-    "AVRobustBench":"AVROBUSTBENCH"
-}
